40cad3d43bdb32a77bb17ada6a9faa5cc79e102e
yassineACE <yassinemedmoum453@gmail.com>
2023-09-15 12:08:58 +0100

    upload first project

---
+++ Desktop/projects/empty.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main ()
+{
+
+      int age;
+      char nom[100],prenom[100] ;
+            printf("donne les information personelle\n");
+
+      printf("qeulle age: \n");
+      scanf("%d",&age);
+      printf("votre age est %d\n", age);
+      printf("quelle nom:\n");
+      scanf("%s",nom);
+      printf("votre nom est%s\n",nom);
+      printf("quelle prenom:\n");
+      scanf("%s",&prenom);
+      printf("votre prenom est %s\n",prenom);
+
+
+
+}
