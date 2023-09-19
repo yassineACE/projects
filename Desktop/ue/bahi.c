13d4935ea4e0eaa8758772b20b4c1c85e73bcd52
yassineACE <yassinemedmoum453@gmail.com>
2023-09-19 17:30:18 +0100

    segnd commit

---
+++ Desktop/ue/bahi.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main (){
+int nombre ;
+printf("entre lo nombre ");
+scanf("%d",& nombre);
+if
+    (nombre > 0 )
+
+printf("le nombre est positive");
+else if
+(nombre < 0)
+printf(" le nombre est negative");
+else
+    printf("nombre est nul");
+
+}
